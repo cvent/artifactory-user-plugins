ddb27259704e6b22e67c131ae9b95317701548fa
Ramez Mourad <rmourad@cvent.com>
2014-06-14 10:19:19 -0400

    Use days instead of months

--- cleanup/artifactCleanup.groovy
+++ cleanup/artifactCleanup.groovy
@@ -1,41 +1,41 @@
 //curl command example for running this plugin. 
-//curl -i -uadmin:password -X POST "http://localhost:8081/artifactory/api/plugins/execute/cleanup?params=months=1|repos=libs-release-local|log|dryRun"
+//curl -i -uadmin:password -X POST "http://localhost:8081/artifactory/api/plugins/execute/cleanup?params=days=7|repos=libs-release-local|log|dryRun"
 executions {
     cleanup() { params ->
-        def months = params['months'] ? params['months'][0] as int: 6
+        def days = params['days'] ? params['days'][0] as int: 6
         def repos = params['repos'] as String[]
         def dryRun = params['dryRun'] ? params['dryRun'][0] as boolean: false
-        artifactCleanup(months, repos, log, dryRun)
+        artifactCleanup(days, repos, log, dryRun)
     }
 }
 
 jobs {
     scheduledCleanup(cron: "0 0 5 ? * 1") {
         def config = new ConfigSlurper().parse(new File("${System.properties.'artifactory.home'}/etc/plugins/artifactCleanup.properties").toURL())
-        artifactCleanup(config.monthsUntil, config.repos as String[], log);
+        artifactCleanup(config.daysUntil, config.repos as String[], log);
     }
 }
 
-private def artifactCleanup(int months, String[] repos, log, dryRun = false) {
-    log.info "Starting artifact cleanup for repositories $repos, until $months months ago"
+private def artifactCleanup(int days, String[] repos, log, dryRun) {
+    log.warn "Starting artifact cleanup for repositories $repos, until $days days ago"
 
-    def monthsUntil = Calendar.getInstance()
-    monthsUntil.add(Calendar.MONTH, -months)
+    def daysUntil = Calendar.getInstance()
+    daysUntil.add(Calendar.DAY_OF_MONTH, -days)
 
     def artifactsCleanedUp =
-        searches.artifactsNotDownloadedSince(monthsUntil, monthsUntil, repos).
+        searches.artifactsNotDownloadedSince(daysUntil, daysUntil, repos).
         each {
             if (dryRun) {
-                log.info "Found $it";
+                log.warn "Found $it";
             } else {
-                log.info "Deleting $it";
+                log.warn "Deleting $it";
                 repositories.delete it
             }
         }
 
     if (dryRun) {
-        log.info "Dry run - nothing deleted. found $artifactsCleanedUp.size artifacts"
+        log.warn "Dry run - nothing deleted. found $artifactsCleanedUp.size artifacts"
     } else {
-        log.info "Finished cleanup, deleted $artifactsCleanedUp.size artifacts"
+        log.warn "Finished cleanup, deleted $artifactsCleanedUp.size artifacts"
     }
 }
